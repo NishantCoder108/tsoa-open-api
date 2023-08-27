5b3ee64df44940243c68711260bcfbb0b6f79b2e
Nishant Kumar <nishantcoder108@gmail.com>
2023-08-27 19:10:09 +0530

    open api ,approx completed

---
+++ readme.md
@@ -0,0 +1,11 @@
+npx openapi-typescript-codegen -i ./build/swagger.json -o node-client -c fetch
+
+yarn run tsoa spec
+
+<!-- Initialize tsconfig.json -->
+
+npx tsc --init
+
+<!-- it just build folder -->
+
+npx tsoa routes
---
+++ src/todos/todoService.ts
@@ -0,0 +1,27 @@
+import { Todo } from "./todo";
+
+//pick ,just from choosing ,that are previous done
+export type TodoCreationParams = Pick<Todo, "title" | "description">;
+
+export class TodosService {
+  public get(todoId: string): Todo {
+    return {
+      id: todoId,
+      title: "Rock Todo",
+      description: " Rock Todo",
+      done: false,
+    };
+  }
+
+  public create(todoCreationParams: TodoCreationParams): Todo {
+    console.log("Rock DB is call");
+
+    return {
+      id: "1",
+      title: "Rock Todo",
+      done: false,
+      description: "Rock Todo",
+      //  ...todoCreationParams
+    };
+  }
+}
